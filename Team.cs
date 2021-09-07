976e461e3ee934048820f157b800a8f31fd1576f
Genderdruid <archonoffail@gmail.com>
2021-09-07 10:36:19 -0700

    initial commit

---
+++ Team.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SOM_Score_Assistant
+{
+    class Team
+    {
+        private string name;
+        private string trigram;
+        private Image logo;
+
+        public Team(string newName, string newTrigram, string logoFilename)
+        {
+            setNames(newName, newTrigram);
+            setLogoFromFilename(logoFilename);
+        }
+
+        public Team() { }
+
+        public void setNames(string newName, string newTrigram)
+        {
+            name = newName;
+            trigram = newTrigram;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getTrigram()
+        {
+            return trigram;
+        }
+        public void setLogoFromFilename(string filename)
+        {
+            logo = Image.FromFile(filename);
+        }
+
+        public Image getLogo()
+        {
+            return logo;
+        }
+    }
+}
---
+++ TextStats.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOM_Score_Assistant
+{
+    /// <summary>
+    /// Abstract class for blocks of text, such as game and fielding information.
+    /// </summary>
+    abstract class TextStats
+    {
+        private Dictionary<string, string> printouts;
+
+        public override string ToString()
+        {
+            string readouts = "";
+
+            foreach(string header in printouts.Keys)
+            {
+                readouts += String.Format("{0}: {1}", header, printouts[header]);
+            }
+
+            return readouts;
+        }
+    }
+}
